889641438d089acd8727f6592bf29c0d7023d033
pineapple789 <pavel_turaev2828@mail.ru>
2022-10-12 17:49:01 +0800

    add binups

---
+++ dataStructure/treeBinups.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <vector>
+
+using ll = long long;
+
+
+struct BinUps
+{
+    std::vector<std::vector<ll>> ej;
+
+    ll getP(ll v, ll k, int i)
+    {
+        if (!k)
+            return v;
+
+        for (; i >= 0; i--)
+        {
+            if ((1 << i) <= k)
+            {
+                return getP(ej[i][v], k - (1 << i), i - 1);
+            }
+        }
+    }
+
+    void build(std::vector<ll> g)
+    {
+        ej.resize(32, std::vector<ll>(g.size()));
+        ej[0] = g;
+
+        for (int i = 1; i < 32; i++)
+        {
+            for (int u = 0; u < g.size(); u++)
+            {
+                ej[i][u] = ej[i - 1][ej[i - 1][u]];
+            }
+        }
+    }
+
+    BinUps(std::vector<ll> g)
+    {
+        build(g);
+    }
+};
